1a871a5702428b659c23cc0402de1628973182bb
Iván Chinchilla <ivanesteban.cc@gmail.com>
2025-11-23 19:52:40 -0600

    Fixing basename issue

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/Portfolio/">
+    <BrowserRouter>
       <ConfigProvider 
         theme={{ 
           algorithm: theme.darkAlgorithm,
